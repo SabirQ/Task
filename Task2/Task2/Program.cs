27f1cc14ca7acff3e0d69440fbfe232ddabfb318
SabirQ <sabirmg@code.edu.az>
2022-02-08 21:15:12 +0400

    Task2_added

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+
+            if (N == 0)
+                Console.WriteLine("reqem ne cut nede tekdir");
+            else if (N % 2 == 0)
+                Console.WriteLine("Reqem cutdur");
+            else
+                Console.WriteLine("Reqem tekdir");
+
+            }
+        }
+    }
+
