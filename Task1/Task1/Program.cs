cc921d844607e9acb79bec3788f13bdf788f912d
SabirQ <sabirmg@code.edu.az>
2022-02-08 20:56:46 +0400

    Task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task 1
+
+            string A = Console.ReadLine();
+
+            if (A.Contains("a"))
+              Console.WriteLine("a herfi var");
+            else
+              Console.WriteLine("a herfi yoxdur");
+
+            
+
+           
+            }
+
+
+
+            
+               
+                
+
+                }
+            }
+
+
+        
+
+    
